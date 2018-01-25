06341ff7fe4f22dc791b35ac34df6eb217359a53
sergey <sergey@nimblecommerce.com>
2018-01-25 17:07:04 +0200

    Support channel

--- src/app/rest/messages.service.ts
+++ src/app/rest/messages.service.ts
@@ -5,33 +5,35 @@ import { MessageDto } from './message.dto';
 
 export class MessagesService {
 	private messages = new Subject<MessageDto[]>();
-	private running = true;
-
-	init() {
-		if (!this.running) {
-			return;
-		}
-		Rest.getMessages()
-			.subscribe(messages => {
-				this.messages.next(messages);
-				setTimeout(
-					() => {
-						this.init();
-					},
-					1000
-				);
-			});
-	}
+	private running = false;
 
 	destroy() {
 		this.running = false;
 	}
 
 	get() {
+		if (this.running) {
+			return this.messages;
+		}
+		this.performIntervalSync();
 		return this.messages;
 	}
 
 	add(message: MessageDto) {
 		return Rest.addMessage(message);
+	}
+
+	private performIntervalSync() {
+		this.running = true;
+		Rest.getMessages()
+			.subscribe(messages => {
+				this.messages.next(messages);
+				setTimeout(
+					() => {
+						this.performIntervalSync();
+					},
+					1000
+				);
+			});
 	}
 }
